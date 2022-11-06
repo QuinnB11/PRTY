6273c3ea3c604ca90f3830b66b697500f1f74629
QuinnB11 <butcher.github@gmail.cm>
2022-11-05 21:17:54 -0400

    Initial Commit

---
+++ PRTY/PRTYApp.swift
@@ -0,0 +1,17 @@
+//
+//  PRTYApp.swift
+//  PRTY
+//
+//  Created by Quinn Butcher on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct PRTYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
